3242f9b5eb130011bd439c5f44a2b637ae76703d
chanaka <chanakaayesh@gmail.com>
2024-07-28 16:18:16 +0530

    added all dao custom classes

---
+++ src/librarymanagement/dao/custome/BorrowingBookDao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package librarymanagement.dao.custome;
+
+import librarymanagement.dao.CrudDao;
+import librarymanagement.entity.BorrowingBookEntity;
+
+/**
+ *
+ * @author ayesh
+ */
+public interface BorrowingBookDao extends CrudDao<BorrowingBookEntity, Integer>{
+    
+}
